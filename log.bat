a7ebdce3406b801b90187dbaca3f337869a12a3e
VinitPania <vinit.pania@cloverinfotech.com>
2023-07-21 17:45:01 +0530

    Added if else condition

--- log.bat
+++ log.bat
@@ -6,13 +6,19 @@ set  month=%DATE:~03,02%
 set num=%DATE:~00,02%
 
 set rev_date=%year%-%month%-%num%
+set log_files=(service.%rev_date%.log  server.log.%rev_date%  wildfly-stderr.%rev_date%.log  wildfly-stdout.%rev_date%.log)
 
 set log_path=E:\devops\wildfly\wildfly-15.0.0.Final\standalone\log
-set tar_path=E:\devops\tar_fold\
+set tar_path=E:\devops\tar_fold
 set path_7zip=C:\"Program Files"\7-Zip\7z.exe
 
-MKDIR %tar_path%\%rev_date%
 
-for  %%x in (service.%rev_date%.log  server.log.%rev_date%  wildfly-stderr.%rev_date%.log  wildfly-stdout.%rev_date%.log) do MOVE  %log_path%\%%x %tar_path%\%rev_date%
+IF not exist %tar_path%\%rev_date% (
+    echo There is no folder so Creating the folder.
+    MKDIR %tar_path%\%rev_date%
+    for  %%x in %log_files% do MOVE %log_path%\%%x %tar_path%\%rev_date%
+    %path_7zip% a -tzip %tar_path%\%rev_date%.zip %tar_path%\%rev_date%\*
+) ELSE (
+    echo Folder Exist so not Creating it.
+)
 
-%path_7zip% a -tzip %tar_path%\%rev_date% %tar_path%
--- loop.bat
+++ loop.bat
@@ -4,9 +4,17 @@ set  month=%DATE:~03,02%
 set num=%DATE:~00,02%
 
 set rev_date=%year%-%month%-%num%
+set tar_path=E:\devops\tar_fold
+set files=(service.%rev_date%.log server.log.%rev_date% wildfly-stderr.%rev_date%.log  wildfly-stdout.%rev_date%.log)
 
 set log_path=E:\devops\wildfly\wildfly-15.0.0.Final\standalone\log
-for %%x in (service.%rev_date%.log server.log.%rev_date% wildfly-stderr.%rev_date%.log  wildfly-stdout.%rev_date%.log) do echo %log_path%\%%x
+
+IF not exist %tar_path%\%rev_date% (
+    echo There is no folder so Creating the folder.
+) ELSE (
+    echo There is  folder so not Creating the folder.
+)
+for %%x in %files% do echo %log_path%\%%x
 
 
 
