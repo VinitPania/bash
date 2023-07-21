b8fafd45fc2091b924844cff1b969685317c1fe2
VinitPania <vinit.pania@cloverinfotech.com>
2023-07-21 16:20:17 +0530

    Changed the log.bat using for loop to extract files

--- log.bat
+++ log.bat
@@ -8,11 +8,11 @@ set num=%DATE:~00,02%
 set rev_date=%year%-%month%-%num%
 
 set log_path=E:\devops\wildfly\wildfly-15.0.0.Final\standalone\log
-set tar_path=E:\devops\tar_fold\%rev_date%
+set tar_path=E:\devops\tar_fold\
 set path_7zip=C:\"Program Files"\7-Zip\7z.exe
 
-mkdir %tar_path%\%rev_date%
+MKDIR %tar_path%\%rev_date%
 
-for  %%x in (service.%rev_date%.log server.log.%rev_date% wildfly-stderr.%rev_date%.log  wildfly-stdout.%rev_date%.log) do move  %log_path%\%%x %tar_path%\
- 
-%path_7zip% a -tzip %tar_path%
+for  %%x in (service.%rev_date%.log  server.log.%rev_date%  wildfly-stderr.%rev_date%.log  wildfly-stdout.%rev_date%.log) do MOVE  %log_path%\%%x %tar_path%\%rev_date%
+
+%path_7zip% a -tzip %tar_path%\%rev_date% %tar_path%
